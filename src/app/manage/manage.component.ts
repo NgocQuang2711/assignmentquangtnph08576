168b6ba7eeb9ef5bed9ecae71d1ab55dc6808a98
NgocQuang2711 <quangtnph08576@fpt.edu.vn>
2020-03-28 00:18:43 +0700

    1 file modified

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from "../Product";
+import { ProductService } from "../product.service";
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-
-  constructor() { }
+  selected: Product;
+  products: Product[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  detailProduct(product) {
+    product.status = !product.status;
+    this.selected = product;
+  }
+  removeItem(id) {
+    this.products = this.products.filter(product => product.id != id);
+  }
+  getProducts() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
