429f8ed8b3943bab5bf360c8ecac6c1e6fbfdb8a
NgocQuang2711 <quangtnph08576@fpt.edu.vn>
2020-03-28 00:33:22 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ManageComponent } from './manage/manage.component';
-import { ShopComponent } from './shop/shop.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ManageComponent } from "./manage/manage.component";
+import { ShopComponent } from "./shop/shop.component";
+import { ManageDetailComponent } from "./manage-detail/manage-detail.component";
+import { ManageEditComponent } from "./manage-edit/manage-edit.component";
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'contact', component: ContactComponent},
-  { path: 'manage', component: ManageComponent},
-  { path: 'shop', component: ShopComponent},
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "manage", component: ManageComponent },
+  { path: "shop", component: ShopComponent },
+  { path: "manage/:productID", component: ManageDetailComponent },
+  { path: "manage/edit/:productID", component: ManageEditComponent }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
